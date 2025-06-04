8ee3141691671ce32a0e6bfbe11bd30b66cc3ea8
Mohanad Ezzat <mohanedezzat0940@gmail.com>
2025-06-04 22:59:30 +0300

    Editing

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const menuRoutes = require('./routes/menuRoutes');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // Middleware
 app.use(express.json());
