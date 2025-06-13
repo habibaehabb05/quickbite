18ef5ba7f2401101477e31a5ebb0ae2858c86025
habibaehabb05 <habiba2309435@miuegypt.edu.eg>
2025-06-13 14:08:10 +0300

    fix: Correct userModel import statement in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,9 +12,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-require("./Models/mydataSchema");
-
-const userModel = mongoose.model("userModel");
+// ✅ استخدمي require بيرجع الموديل نفسه
+const userModel = require("./Models/mydataSchema");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
