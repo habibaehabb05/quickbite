cdf4cef65f29e85753e2dc8eb2f7ff46238dc63c
Mohanad Ezzat <mohanedezzat0940@gmail.com>
2025-06-13 19:28:48 +0300

    fix: Remove unnecessary space before mongoose import in menu.js

--- backend/Models/menu.js
+++ backend/Models/menu.js
@@ -1,11 +1,10 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const MenuItemSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
-    
     description: {
         type: String,
         required: true
